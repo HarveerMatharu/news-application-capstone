e361069da4945d785a913d189c2fc373b45150b5
Harveer <harveer.matharu@icloud.com>
2026-01-02 15:52:10 +0000

    Restore MariaDB database configuration

--- news_project/settings.py
+++ news_project/settings.py
@@ -84,6 +84,7 @@ WSGI_APPLICATION = 'news_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -99,6 +100,13 @@ DATABASES = {
     }
 }
 
+# SQLite configuration (for Sphinx documentation)
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
